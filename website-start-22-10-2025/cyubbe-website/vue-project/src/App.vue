2a0fc5db4747a796694b92f09cf461ac82aff1e2
NikiDivis <divis.bindestrich@gmail.com>
2025-10-22 15:17:12 +0200

    Test?

--- website-start-22-10-2025/cyubbe-website/vue-project/src/App.vue
+++ website-start-22-10-2025/cyubbe-website/vue-project/src/App.vue
@@ -1,11 +1,16 @@
-<script setup></script>
+<script setup>
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+
+  <h1>Hello and Welcome to this very empty page!</h1>
+
 </template>
 
-<style scoped></style>
+<style scoped>
+:global(body){
+  background-image: url("https://images.unsplash.com/photo-1712145176570-6cb1d98a126a?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1032");
+  background-color: #FEEBE7;
+}
+</style>
